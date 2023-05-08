624fd1abb4dd3cbef7bd9f53d2192d0809520aa5
ptan01 <ptanjim01@gmail.com>
2023-05-08 18:02:36 +0600

    app delete route set and delete single data from mongodb

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000 ;
@@ -42,6 +42,13 @@ async function run() {
         console.log(coffee)
         const result = await coffeeCollection.insertOne(coffee)
         res.send(result)
+    })
+
+    app.delete('/coffee/:id' , async(req , res )=>{
+      const id = req.params.id ;
+      const query = { _id : new ObjectId(id)}
+      const result = await coffeeCollection.deleteOne(query)
+      res.send(result)
     })
 
 
